567e2247ef61096f5695fc422fdc226359c7afa9
James Green <jkg@earth.li>
2014-08-07 23:26:56 +0100

    First basically "working" commit, except the AJAX bit. Which is all of it. Bah.

--- lib/App/NameVoter.pm
+++ lib/App/NameVoter.pm
@@ -1,10 +1,34 @@
 package App::NameVoter;
 use Dancer ':syntax';
+use Dancer::Plugin::Database;
+use Dancer::Plugin::Ajax;
 
-our $VERSION = '0.1';
+use JSON::Any;
+
+our $VERSION = '0.001';
 
 get '/' => sub {
     template 'index';
 };
 
-true;
+post '/login' => sub {
+	if ( params->{name} ) {
+		session user => params->{name};
+		redirect '/vote';
+	}
+};
+
+get '/vote' => sub {
+	redirect '/' unless session('user');
+	template 'vote', { name => session('user') };
+};
+
+ajax '/show' => sub {
+
+};
+
+ajax '/save' => sub {
+
+};
+
+'0 but true';
